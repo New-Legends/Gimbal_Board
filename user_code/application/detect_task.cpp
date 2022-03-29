a8ba2bad1ac1d8ebec76cd47fdb32063eaeb14a6
DZK_123 <k63481116@gmail.com>
2022-03-29 10:42:09 +0800

    3.29

--- user_code/application/detect_task.cpp
+++ user_code/application/detect_task.cpp
@@ -279,9 +279,8 @@ static void detect_init(uint32_t time)
     }
 
     error_list[OLED_TOE].data_is_error_fun = NULL;
-//    error_list[OLED_TOE].solve_lost_fun = OLED_com_reset;
     error_list[OLED_TOE].solve_data_error_fun = NULL;
-
+//    error_list[OLED_TOE].solve_lost_fun = OLED_com_reset;
 //    error_list[DBUSTOE].dataIsErrorFun = RC_data_is_error;
 //    error_list[DBUSTOE].solveLostFun = slove_RC_lost;
 //    error_list[DBUSTOE].solveDataErrorFun = slove_data_error;
